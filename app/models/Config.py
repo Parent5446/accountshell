225713bb9d6d152db00ae3efb462088e597b2dc7
Tyler Romeo <tylerromeo+github@gmail.com>
2009-03-17 21:53:49 -0400

    Adding draft Config and Database models.

---
+++ app/models/Config.py
@@ -0,0 +1,37 @@
+import re
+
+class Config():
+	"""A class to maintain the program's configuration options."""
+	data = {}
+	def __init__(self, initOptions):
+		self.updateFromFile()
+		for key in initOptions.keys():
+     			self.data[key] = initOptions[key]
+	def getOption(self, key):
+		return self.data[key]
+	def putOption(self, key, value):
+		self.data[key] = value
+	def updateFromFile(self, filename = ''):
+		if filename == '':
+			global ROOTPATH
+			filename = ROOTPATH + 'data/config'
+		configfile = open(filename, 'r')
+		rawoptions = configfile.readlines()
+		configfile.close()
+		temp = {}
+		for rawoption in rawoptions:
+			separated = re.split('\=', rawoption)
+			temp[separated[0]] = separated[1]
+		self.data = temp
+		return true
+	def updateToFile(self, filename = ''):
+		if filename == '':
+			global ROOTPATH
+			filename = ROOTPATH + 'data/config'
+		configfile = open(filename, 'w')
+		temp = []
+		for key in self.data.keys():
+			temp.append(key + "=" + self.data[key])
+		configfile.writelines(temp)
+		configfile.close()
+		return true
---
+++ app/models/Database.py
@@ -0,0 +1,38 @@
+import re
+
+class Database():
+	"""A class to maintain the program's configuration options."""
+	data = []
+	def __init__(self, config):
+		self.config = config
+		self.updateFromFile()
+	def addLine(self, information):
+		self.data.append(information)
+		self.updateToFile()
+		return true
+	def updateFromFile(self):
+		datafile = open(filename = self.getRequestFilename(), 'r')
+		lines = configfile.readlines()
+		data.close()
+		temp = {}
+		for line in lines:
+			templine = {}
+			separated1 = re.split('\;', line)
+			for option in separated:
+				separated2 = re.split('\=', option)
+				templine[separated2[0]] = separated2[1]
+			self.data.append(templine)
+		return true
+	def updateToFile(self):
+		datafile = open(filename = self.getRequestFilename(), 'w')
+		temp = []
+		for dataset in self.data:
+			line = ''
+			for key in dataset.keys():
+				line = line + key + '=' + dataset[key] + ';'
+			temp.append(line)
+		datafile.writelines(temp)
+		datafile.close()
+		return true
+	def getRequestFilename():
+		return self.config.getOption('database.filename')
